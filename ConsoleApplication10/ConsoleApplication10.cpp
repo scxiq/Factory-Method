1d99e179c36029e83ee0a1a13f2978f157f39f77
cyber <cyber@ADMIN-PC>
2024-01-25 13:48:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+// Абстрактний клас продукту
+class Product {
+public:
+    virtual void display() = 0;
+};
+
+// Конкретний клас продукту
+class ConcreteProduct : public Product {
+public:
+    void display() override {
+        std::cout << "Concrete Product\n";
+    }
+};
+
+// Абстрактний клас фабрики
+class Creator {
+public:
+    virtual Product* createProduct() = 0;
+};
+
+// Конкретна фабрика, яка створює конкретний продукт
+class ConcreteCreator : public Creator {
+public:
+    Product* createProduct() override {
+        return new ConcreteProduct();
+    }
+};
+
+int main() {
+    // Використання фабричного методу
+    Creator* creator = new ConcreteCreator();
+    Product* product = creator->createProduct();
+    product->display();
+
+    delete product;
+    delete creator;
+
+    return 0;
+}
